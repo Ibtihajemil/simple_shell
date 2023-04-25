674be39d13d5849d78492a1fc98d3e062dbd6487
ibtihajemil <ibtihajemil@gmail.com>
2023-04-25 16:30:57 -0700

    first commit

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * area - calculates the area of a square
+ * @num1: the length of the square
+ * @num2: the width of the square
+ * Return: the area
+ */
+int area(int num1, int num2)
+{
+	return (num1 * num2);
+}
